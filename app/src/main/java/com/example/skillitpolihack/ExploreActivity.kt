1f3f86d1f854875c80d2651cb6961b9c71f3cd21
Petru <petru.zdroba@gmail.com>
2022-12-10 18:45:41 +0200

    -

--- app/src/main/java/com/example/skillitpolihack/ExploreActivity.kt
+++ app/src/main/java/com/example/skillitpolihack/ExploreActivity.kt
@@ -5,23 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
-class HomeActivity :AppCompatActivity(){
+class ExploreActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_activity)
+        setContentView(R.layout.explore_activity)
         val explore_button = findViewById<CardView>(R.id.explore_button) as CardView
         val profile_button = findViewById<CardView>(R.id.profile_button) as CardView
         val home_button = findViewById<CardView>(R.id.home_button) as CardView
-        home_button.setOnClickListener(){
+        home_button.setOnClickListener{
             intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
-        explore_button.setOnClickListener(){
+        explore_button.setOnClickListener{
             intent = Intent(this, ExploreActivity::class.java)
             startActivity(intent)
         }
-        profile_button.setOnClickListener(){
+        profile_button.setOnClickListener{
             intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
